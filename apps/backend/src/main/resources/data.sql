b1e00e64d4d886b7d7a353426715334f9115e422
JP <Joshua.Presland@proton.me>
2025-11-22 17:17:04 +0000

    Product Description
    - Added 'description' column to SQL schema
    - Added 'description' to DTO
    - Set some place holder descriptions

--- apps/backend/src/main/resources/data.sql
+++ apps/backend/src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO product (id, sku, price)
-VALUES (DEFAULT, 'AAA001', 5.99),
-       (DEFAULT, 'AAA002', 10.00),
-       (DEFAULT, 'BBB001', 0.69);
+INSERT INTO product (id, sku, price, description)
+VALUES (DEFAULT, 'AAA001', 5.99, 'Standard Widget'),
+       (DEFAULT, 'AAA002', 10.00, 'Advanced Widget'),
+       (DEFAULT, 'BBB001', 0.69, 'Basic Gizmo');
 
 INSERT INTO customer (id, name)
 VALUES (DEFAULT, 'Rick Sanchez'),
