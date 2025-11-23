a77d2b56c97e6b026742f406166f99fcad38a735
JP <Joshua.Presland@proton.me>
2025-11-23 15:14:10 +0000

    3. Customers
    3.1
    - Added table to display customer data
    - Added ability to add new customers
    - Added ability to edit customers
    
    3.2
    - Added customer addresses
    - Created postcode format validation

--- apps/backend/src/main/resources/data.sql
+++ apps/backend/src/main/resources/data.sql
@@ -3,7 +3,7 @@ VALUES (DEFAULT, 'AAA001', 5.99, 'Standard Widget'),
        (DEFAULT, 'AAA002', 10.00, 'Advanced Widget'),
        (DEFAULT, 'BBB001', 0.69, 'Basic Gizmo');
 
-INSERT INTO customer (id, name)
-VALUES (DEFAULT, 'Rick Sanchez'),
-       (DEFAULT, 'Walter White'),
-       (DEFAULT, 'William Butcher');
+INSERT INTO customer (id, name, house, street, city, postcode)
+VALUES (DEFAULT, 'Buckingham Palace', 'Buckingham Palace', 'The Mall', 'London', 'SW1A 1AA'),
+       (DEFAULT, '10 Downing Street', '10', 'Downing Street', 'London', 'SW1A 2AA'),
+       (DEFAULT, 'The British Museum', 'The British Museum', 'Great Russell St', 'London', 'WC1B 3DG');
